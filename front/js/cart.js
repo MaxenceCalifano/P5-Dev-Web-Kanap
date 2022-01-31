1f60a70f29ad009c40d1362b7715dac402ffdd8f
Maxence <maxence.califano@gmail.com>
2022-01-31 17:59:08 +0100

    Fetch only product needed
    Better use of API, fetch with promise all

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,8 +4,8 @@ const totalPrice = document.getElementById("totalPrice");
 let cartTotalQuantity = 0;
 let cartTotalPrice = 0;
 let cart = JSON.parse(localStorage.getItem("cart"));
-let products;
-fetch("http://localhost:3000/api/products/")
+let products = "";
+/* fetch("http://localhost:3000/api/products/")
   .then(function (res) {
     if (res.ok) {
       return res.json();
@@ -13,57 +13,64 @@ fetch("http://localhost:3000/api/products/")
   })
   .then(function (value) {
     //products = value;
-    console.log("produits de l'API", value);
+    console.log("produits de l'API", value); */
+cart.map((item) =>
+  console.log(`http://localhost:3000/api/products/${item.id}`)
+);
+Promise.all(
+  cart.map((item) =>
+    fetch(`http://localhost:3000/api/products/${item.id}`).then((res) =>
+      res.json()
+    )
+  )
+)
+  .then((json) => {
+    console.log(json[0]);
+    for (let i = 0; i < cart.length; i++) {
+      cart[i].price = json[i].price;
+      cart[i].name = json[i].name;
+      cart[i].imageUrl = json[i].imageUrl;
+      cart[i].altTxt = json[i].altTxt;
+      cartTotalPrice += cart[i].price * cart[i].quantity;
+      cartTotalQuantity += parseInt(cart[i].quantity);
 
-    for (const item of cart) {
-      console.log("éléments dans le panier", item);
-      cartTotalQuantity += parseInt(item.quantity);
-      for (const product of value) {
-        if (product._id == item.id) {
-          item.price = product.price;
-          item.name = product.name;
-          item.imageUrl = product.imageUrl;
-          item.altTxt = product.altTxt;
-          cartTotalPrice += item.price * item.quantity;
-        }
-      }
-      cartItems.insertAdjacentHTML(
-        "beforeend",
-        `<article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-        <div class="cart__item__img">
-          <img src=${item.imageUrl} alt=${item.altTxt}>
+      products += `<article class="cart__item" data-id="${cart[i].id}" data-color="${cart[i].color}">
+      <div class="cart__item__img">
+        <img src=${cart[i].imageUrl} alt=${cart[i].altTxt}>
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${cart[i].name}</h2>
+          <p>${cart[i].color}</p>
+          <p>${cart[i].price}€</p>
         </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>${item.name}</h2>
-            <p>${item.color}</p>
-            <p>${item.price}€</p>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart[i].quantity}"  >
           </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}"  >
-            </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
-            </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
           </div>
         </div>
-      </article>`
-      );
+      </div>
+    </article>`;
     }
+  })
+  .then(() => {
+    cartItems.insertAdjacentHTML("beforeend", products);
     totalQuantity.textContent = cartTotalQuantity;
     totalPrice.textContent = cartTotalPrice;
 
-    const articles = Array.from(document.getElementsByClassName("cart__item"));
+    const articles = Array.from(document.getElementsByClassName("cart__item")); // Get all articles and listen the value of quantity input
 
     articles.forEach((article) => {
-      article.addEventListener("change", (event) =>
-        cartQuantitychanged(article, event)
-      );
+      article.addEventListener("change", (event) => {
+        cartQuantitychanged(article, event);
+      });
     });
 
-    const deleteButtons = document.getElementsByClassName("deleteItem");
+    const deleteButtons = document.getElementsByClassName("deleteItem"); // Get all delete buttons and listen click
 
     for (let i = 0; i < deleteButtons.length; i++) {
       let articleToDelete = deleteButtons[i].closest("article");
@@ -72,14 +79,8 @@ fetch("http://localhost:3000/api/products/")
         deleteArticle(articleToDelete)
       );
     }
-
-    /* let cartWithoutPrice = [...cart];
-    cartWithoutPrice.forEach((test12) => delete test12.price);
-    console.log(cartWithoutPrice); */
   })
-  .catch(function (err) {
-    console.error(err);
-  });
+  .catch((err) => console.error(err));
 
 const cartQuantitychanged = (article, event) => {
   let getId = article.dataset.id;
@@ -128,10 +129,6 @@ const testUserInput = (testString, testRegex, errMsgPlaceholder, errorMsg) => {
     errMsgPlaceholder.textContent = errorMsg;
     contact[testString.name] = "";
   }
-
-  /* testRegex.test(testString)
-    ? (errMsgPlaceholder.textContent = "")
-    : (errMsgPlaceholder.textContent = errorMsg); */
 };
 
 const errPhrase = (field) =>
@@ -185,7 +182,7 @@ email.addEventListener("blur", (e) =>
     email,
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
     emailErrorMsg,
-    errPhrase("e-mail")
+    "l'addresse e-mail entrée semble invalide, veuillez la vérifier s'il vous plait"
   )
 );
 
